c452681b4012cb4eb49bdc1a3df7f2ac4ad6a42d
Steve Garrison <stevegarrison@Steves-MacBook-Pro.local>
2019-04-10 20:59:31 -0600

    Pushing final Functionality MVP

---
+++ app/data/friends.js
@@ -0,0 +1,117 @@
+var friendsArray = [
+	{
+		name: 'Michael',
+		photo: 'https://i.pinimg.com/originals/a5/62/97/a56297e2d8aa41bcb3b44962b670dc55.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jim',
+		photo: 'https://nypdecider.files.wordpress.com/2017/11/jim-halpert.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dwight',
+		photo: 'https://gogetnotary.com/wp-content/uploads/2018/07/dwight-800x800.jpeg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Pam',
+		photo: 'https://coedmagazine.files.wordpress.com/2018/01/pam-the-office1.jpg?quality=88&w=640',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Oscar',
+		photo: 'https://img.nbc.com/mpx-static/NBCdotCOM/mezzthumb/def047f1039c2087301abf875242223b_1c693cd0f85337c5e548b89bb761cb07.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Kevin',
+		photo: 'https://i.ytimg.com/vi/g8LmF2QFduY/hqdefault.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Meredith',
+		photo: 'https://i.pinimg.com/originals/af/43/5c/af435c6bdf2fd5cf720f97b1046fe2c0.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+];
+
+module.exports = friendsArray;
+ 
