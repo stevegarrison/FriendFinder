4c22f3158f0a408ed02cd439fab31a8b7bae64bf
Steve Garrison <stevegarrison@Steves-MacBook-Pro.local>
2019-04-10 21:20:55 -0600

    Pushing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    // app.get("*", function(req, res) {
-    //     res.sendfile(path.join(__dirname, "../public/home.html"));
-    // })
+    app.get("*", function(req, res) {
+        res.sendfile(path.join(__dirname, "../public/home.html"));
+    })
 }
